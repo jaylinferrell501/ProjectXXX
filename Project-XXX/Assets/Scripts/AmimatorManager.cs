3890b67fa486fc5f416592983dda90a09b8f6a2a
jaylinferrell501 <54218978+jaylinferrell501@users.noreply.github.com>
2022-09-08 20:42:29 -0700

    ADD NEW FEATURES

---
+++ Project-XXX/Assets/Scripts/AmimatorManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmimatorManager : MonoBehaviour
+{
+    // Our Animator reference variable
+    Animator animator;
+    float snappedHorizontal;
+    float snappedVertical;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void HandleAnimatorValues(float horizontalMovement, float verticalMovement)
+    {
+        if (horizontalMovement > 0)
+        {
+            snappedHorizontal = 1;
+        }
+        else if (horizontalMovement < 0)
+        {
+            snappedHorizontal = -1;
+        }
+        else
+        {
+            snappedHorizontal = 0;
+        }
+
+        if (verticalMovement > 0)
+        {
+            snappedVertical = 1;
+        }
+        else if (verticalMovement < 0)
+        {
+            snappedVertical = -1;
+        }
+        else
+        {
+            snappedVertical = 0;
+        }
+
+        animator.SetFloat("Horizontal", snappedHorizontal, 0.1f, Time.deltaTime);
+        animator.SetFloat("Vertical", snappedVertical, 0.1f, Time.deltaTime);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
